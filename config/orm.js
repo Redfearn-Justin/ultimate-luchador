a27eb3a27a398761d2c187304dd3dba167cdbb08
GBaxx <gav.bax@gmail.com>
2018-10-25 08:44:05 -0700

    File structure

---
+++ config/orm.js
@@ -0,0 +1,58 @@
+// Import MySQL connection.
+var connection = require("./connection.js");
+
+// Helper function for SQL syntax - formats "?" better: ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+};
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+};
+
+// ORM Object with MySQL methods
+// =============================
+var orm = {
+
+    // EXAMPLE METHOD
+    selectTen: (tableName, stat, cb) => {
+
+        console.log("selectTen");
+
+        var queryString = "SELECT * FROM " + tableName + " ORDER BY " + stat +" DESC LIMIT 10;";
+
+        connection.query(queryString, function (err, res) {
+
+            if (err) throw err;
+
+            cb(res);
+
+        });
+    }
+};
+// =============================
+
+// Export the orm object
+module.exports = orm;
---
+++ controllers/controller.js
@@ -0,0 +1,33 @@
+// Require models.js file
+var models = require('../models/models.js');
+
+// Require express and build our router instance
+var express = require("express");
+var router = express.Router();
+
+// HTML ROUTES 
+// *************************************************
+
+router.get("/", function (req, res) {
+    res.render("index");
+});
+
+// API ROUTES 
+// *************************************************
+
+router.post("/api/newchar", function (req, res) {
+    models.create([
+        'captain_name',
+        'ship_name'
+    ], [
+            req.body.captain_name,
+            req.body.ship_name
+        ], function (result) {
+            var resid = result.insertId;
+            idHolder = resid;
+            res.json({ id: resid });
+        });
+});
+
+// EXPORT OUR ROUTER
+module.exports = router;
